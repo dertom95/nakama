b8d61be5703949b45e8c8ed092e173bd950b8665
Thomas Trocha <thomas.trocha@gmail.com>
2019-11-22 01:28:41 +0000

    some dockerfiles for arm

---
+++ build/arm64v8/build_nakama_alpine_2.7.0.sh
@@ -0,0 +1,1 @@
+sudo docker build -f Dockerfile-arm64v8-alpine /home/ttrocha/_dev/extprojects/nakama/build/arm64v8 --build-arg commit=v2.7.0 --build-arg version=v2.7.0 -t dertom95/nakama:2.7.0-arm64v8
